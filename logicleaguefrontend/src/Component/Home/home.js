97e0206ea3e363d88c1434169d0711279a35a622
Ruddarm <ruddarmmaurya@gmail.com>
2024-11-24 20:09:12 +0530

    Adding headder

--- logicleaguefrontend/src/Component/Home/home.js
+++ logicleaguefrontend/src/Component/Home/home.js
@@ -1,6 +1,7 @@
 import React, { PureComponent, useState, useEffect } from "react";
 import axios from "axios";
 import axiosInstance from "../utils/request";
+import Header  from "../utils/header";
 function HomePage() {
   const [data, setdata] = useState("");
   const user = JSON.parse(localStorage.getItem("user"));
@@ -17,6 +18,7 @@ function HomePage() {
 
   return (
     <>
+      <Header></Header>
       <h1>Hello...{user.username}</h1>
       <h2>{key ? "hello Drlng.. I love you " : data}&#10084;</h2>
     </>
